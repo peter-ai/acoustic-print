bfdb3bceec73794b0b0b127c42a23e3a6c9e417e
Peter Akioyamen <peter.akioyamen@gmail.com>
2023-10-09 18:01:54 -0400

    Finalized album page

--- src/pages/3_💿_Album.py
+++ src/pages/3_💿_Album.py
@@ -2,12 +2,15 @@
 Author: Peter Akioyamen
 Github: @peter-ai
 
-A web application which uses audio feature data from the Free Music Archive (FMA; 2018)
-to create visual acoustic fingerprints of songs and compare audio characteristics across the
-music catalogue. The app uses a MySQL backend and is built on Streamlit.
+A web application which uses audio feature data from the Free Music Archive (FMA)
+to create visual acoustic fingerprints of songs and compare audio characteristics 
+across the music catalogue. The app is built on Streamlit.
 
 Album Page of the Acoustic Print web app
 """
+# import standard libraries
+import time
+
 # import external dependencies
 import numpy as np
 import pandas as pd
@@ -18,6 +21,7 @@ from sklearn.metrics.pairwise import cosine_distances
 from acoustic_helpers import (
     get_audio_descriptions,
     get_filtered_acoustics,
+    get_logger,
     get_sql_connect_str,
     plot_acoustic_bars,
     plot_acoustic_radar,
@@ -32,14 +36,19 @@ def main():
     )
 
     try:
+        ### ---------------------------------------
+        ### Retrieve data and visualize album stats
+        # get id of the request album via url query parameters
         ids = st.experimental_get_query_params()["id"]
         id = int(ids[0])
         if len(ids) > 1:
             st.caption("Only one album can be shown at a time.")
 
+        # connect to database
         conn_str = get_sql_connect_str()
         conn = st.experimental_connection(name="acoustic_db", type="sql", url=conn_str)
 
+        # get discography/track list for requested album
         discography = conn.query(
             f"""
             SELECT Ab.favorites AS Favorites, Ab.listens AS Listens, T.favorites AS `Track Favorites`, T.listens as `Track Listens`,
@@ -59,9 +68,14 @@ def main():
             WHERE Ab.id={id};
             """
         )
+
         # if album has no tracks, raise exception
-        if discography.shape[0] == 0 or discography["Release Date"].isna().iloc[0]:
-            raise ValueError
+        if discography.shape[0] == 0:
+            raise ValueError(f"Album id={id}; requested an album with no songs.")
+        if discography["Release Date"].isna().iloc[0]:
+            raise ValueError(
+                f"Album id={id}; requested an album with no documented release date."
+            )
 
         # display album info
         album_df = discography.filter(
@@ -88,6 +102,7 @@ def main():
             ]
         ).mean(axis=0)
 
+        # process genres and get album stats aggregated by tracks within their associated genres
         genres_ids = tuple(discography.genre_id.unique())
         genres = tuple(discography.Genre.unique())
         albums_by_genre = conn.query(
@@ -117,12 +132,15 @@ def main():
         )
         genre_stats_df["id"] = "1"
 
+        # begin visualizations
         tab1, tab2 = st.tabs(["Comparative Radar", "Comparative Bar"])
         with tab1:
+            # get aggregated acoustic information based on the selected track's associate genres
             radar_df = get_filtered_acoustics(
                 album_stats_df, genre_stats_df, genres=genres, songs=False
             )
 
+            # plot comparative acoustic radar
             st.write("Audio Features of Current Album vs. Albums in Associated Genres")
             st.caption(
                 "",
@@ -132,6 +150,7 @@ def main():
             plot_acoustic_radar(radar_df)
 
         with tab2:
+            # rename datasets
             bar_df = radar_df.copy(deep=True)
             bar_df.columns = [
                 "Audio Features",
@@ -147,10 +166,15 @@ def main():
             )
             plot_acoustic_bars(bar_df)
 
-        # recommendation system
+        ### ---------------------
+        ### Recommendation System
         with st.sidebar:
+            # init empy dict for recommendations already seen across genres
             recommendations = {}
+
+            # for each genre associated with tracks in the requested album
             for i, genre in enumerate(genres):
+                # style bullet sections
                 if i == 0:
                     st.write(
                         f"""<ul style="margin-bottom:0px;">Similar {genre} Albums:</ul>""",
@@ -161,7 +185,8 @@ def main():
                         f"""<ul style="margin-top:1rem; margin-bottom:0px;">Similar {genre} Albums:</ul>""",
                         unsafe_allow_html=True,
                     )
-                # filter for necessary data
+
+                # filter for relevant albums given the particular genre
                 recs_stats_df = albums_by_genre.loc[
                     albums_by_genre.Genre == genre, :
                 ].drop(["Genre", "Tempo", "Duration"], axis=1)
@@ -170,22 +195,24 @@ def main():
                 ).set_index("id")
                 recs_stats_df = recs_stats_df.drop(["id", "Title", "Artist"], axis=1)
 
-                # compute cosine distance
+                # compute cosine distance between albums in genre and current album
                 cos_dis = cosine_distances(
                     recs_stats_df, album_stats_df.to_numpy().reshape(1, -1)
                 ).reshape(
                     -1,
                 )
 
-                # get albums with minimum cosine distances
+                # get albums with minimum cosine distances/max cosine similarity
                 genre_recs = recs_details_df.iloc[
                     np.argpartition(cos_dis, 3)[:3], :
                 ].reset_index(drop=False)
 
-                # write recommendation list
+                # output recommendation list for the genre
                 genre_recs.apply(write_recs, axis=1, recommendations=recommendations)
 
-        # display info about tracks on album
+        ### ------------------------------------
+        ### Display track list for current album
+        # get relevant columns to display for tracks
         tracks_df = (
             discography.filter(
                 ["Song", "Duration", "Track Favorites", "Track Listens", "Explicit"]
@@ -195,16 +222,21 @@ def main():
             )
             .drop_duplicates()
         )
+
+        # format the values in columns for display
         tracks_df["Duration"] = tracks_df.Duration.apply(
             lambda x: f"{x//60}:{x-((x//60)*60):02d}"
         )
         tracks_df["Explicit"] = tracks_df.Explicit.apply(
             lambda x: "Ambiguous" if x == -1 else "No" if x == 0 else "Yes"
         )
+
+        # display track list
         st.write("Track List")
         st.dataframe(tracks_df, use_container_width=True, hide_index=True)
 
-        # create audio feature description table
+        ### -----------------------------
+        ### Descrpition of audio features
         feature_desc = get_audio_descriptions()
         st.divider()
         with st.expander("Description of audio features"):
@@ -229,22 +261,82 @@ def main():
                 hide_index=True,
             )
 
+    ### ------------------------------
+    ### Exception catching and logging
     except KeyError as e:
-        st.write(
-            "Oops, seems like the album you've selected is missing from the catalogue, please select another..."
-        )
-        st.stop()
+        # get logger and log exception
+        logger = get_logger()
+        logger.error(repr(e), exc_info=True)
+
+        # display user friendly message about error
+        t = st.empty()
+        for i in range(5, 0, -1):
+            t.error(
+                f"""
+                ## Error 404 Not Found
+                
+                Oops, seems like the album you've selected is missing 
+                from the catalogue, please select another...
+
+                Redirecting to homepage in {i}...
+                """,
+                icon="❌",
+            )
+            time.sleep(1)
+
+        # redirect back to the home page
+        nav_script = f"""<meta http-equiv="refresh" content="0; url='/'">"""
+        st.write(nav_script, unsafe_allow_html=True)
+
     except ValueError as e:
-        st.write(e)
-        st.write(
-            "Oops, seems like someone tinkered with the album identifier, it's invalid so please select another..."
-        )
-        st.stop()
+        # get logger and log exception
+        logger = get_logger()
+        logger.error(repr(e), exc_info=True)
+
+        # display user friendly message about error
+        t = st.empty()
+        for i in range(5, 0, -1):
+            t.error(
+                f"""
+                ## Error 404 Not Found
+                
+                Oops, seems like something went wrong, the album request is
+                invalid or does not exist so please select another.
+
+                Redirecting to homepage in {i}...
+                """,
+                icon="❌",
+            )
+            time.sleep(1)
+
+        # redirect back to the home page
+        nav_script = f"""<meta http-equiv="refresh" content="0; url='/'">"""
+        st.write(nav_script, unsafe_allow_html=True)
+
     except Exception as e:
-        st.write(
-            "Oops, seems like the album you selected has no tracks on it, plase select another..."
-        )
-        st.stop()
+        # get logger and log exception
+        logger = get_logger()
+        logger.error(repr(e), exc_info=True)
+
+        # display user friendly message about error
+        t = st.empty()
+        for i in range(5, 0, -1):
+            t.error(
+                f"""
+                ## Error 404 Not Found
+                
+                Oops, seems like the album you selected has no tracks on it, 
+                please select another...
+
+                Redirecting to homepage in {i}...
+                """,
+                icon="❌",
+            )
+            time.sleep(1)
+
+        # redirect back to the home page
+        nav_script = f"""<meta http-equiv="refresh" content="0; url='/'">"""
+        st.write(nav_script, unsafe_allow_html=True)
 
 
 # main program
